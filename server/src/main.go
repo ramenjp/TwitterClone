227752a1b68e09c1686c67741c157298e8fdabb7
hrb-takagi-yosuke <takagi.yosuke@hrbrain.co.jp>
2020-12-31 18:48:46 +0900

    gormでMySQLと接続

--- server/src/main.go
+++ server/src/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type User struct {
+	ID    int    `gorm:"id"`
+	Name  string `db:"name"`
+	Email string `db:"email"`
+}
+
 func main() {
+	db := gormConnect()
+	defer db.Close()
+
 	engine := gin.Default()
 
 	//CORS設定
@@ -39,9 +51,32 @@ func main() {
 	//ルーティング
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+			"message": "Hello world",
 		})
 	})
 
+	engine.GET("/test", func(c *gin.Context) {
+
+	})
+
 	engine.Run(":2001")
+}
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "root"
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "Twitter"
+
+	CONNECT := USER + ":" + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
+	db.AutoMigrate(&User{})
+
+	fmt.Println("SQL connecting...", USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
